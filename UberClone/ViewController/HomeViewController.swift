7706abb8ed9a79dfbcf8f78b6d309b04e6f18227
Hyeonjoonn <phj3046@gmail.com>
2023-05-09 18:59:20 +0900

    feat: Mapkit 위치정보 표시

--- UberClone/ViewController/HomeViewController.swift
+++ UberClone/ViewController/HomeViewController.swift
@@ -9,19 +9,22 @@ import UIKit
 import SnapKit
 import FirebaseAuth
 import MapKit
+import CoreLocation
 
 class HomeViewController: UIViewController {
 
 //MARK: - UI Components
     private let mapView = MKMapView()
-    
+    // 위치 묻는 역할
+    private let locationManager = CLLocationManager()
     
 //MARK: - Life cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        signOut()
+//        signOut()
         view.backgroundColor = .backgroundColor
         setUIandConstraints()
+        enableLocationServices()
     }
     
 //MARK: - Firebase API
@@ -34,13 +37,54 @@ class HomeViewController: UIViewController {
         }
     }
     
-//MARK: - Helper
+//MARK: - set UI
     func setUIandConstraints() {
+        configureMapView()
+    }
+    
+    func configureMapView() {
         view.addSubview(mapView)
         mapView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
+        
+        self.mapView.showsUserLocation = true
+        self.mapView.setUserTrackingMode(.follow, animated: true)
     }
     
+//MARK: - Helper
     
+    
+    
+}
+
+extension HomeViewController: CLLocationManagerDelegate {
+    func enableLocationServices() {
+        locationManager.delegate = self
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            print("DEBUG: 위치 정보 없을 때 권한 정보 물음")
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            break
+        case .authorizedAlways:
+            print("DEBUG: 항상 위치정보 권한 사용중")
+            locationManager.startUpdatingLocation()
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        case .authorizedWhenInUse:
+            print("DEBUG: 앱을 사용할 때만 위치정보 권한 사용중")
+            locationManager.requestAlwaysAuthorization()
+        @unknown default:
+            break
+        }
+    }
+    
+    // 권한 부여 상태 변경 시
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status == .authorizedWhenInUse {
+            locationManager.requestAlwaysAuthorization()
+        }
+    }
 }
