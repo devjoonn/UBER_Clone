3434ab7ef8057f54db60eee692c63cd6d86c3c88
Hyeonjoonn <phj3046@gmail.com>
2023-06-24 15:31:33 +0900

    feat: Trip init() 현재 위치 위도 경도 pickupCoordinates에 의존성 주입

--- UberClone/Model/Trip.swift
+++ UberClone/Model/Trip.swift
@@ -8,10 +8,28 @@
 import CoreLocation
 
 struct Trip {
-    let pickupCoordinates: CLLocationCoordinate2D
+    var pickupCoordinates: CLLocationCoordinate2D
     let destinationCoordinates: CLLocationCoordinate2D
     let passengerUid: String
     var driverUid: String?
+    var state: TripState!
     
+    init(passengerUid: String, dictionary: [String: Any]) {
+        self.passengerUid = passengerUid
+         
+        if let pickupCoordinates = dictionary["pickupCoordinates"] as? NSArray {
+            guard let lat = pickupCoordinates[0] as? CLLocationDegrees else { return }
+            guard let long = pickupCoordinates[1] as? CLLocationDegrees else { return }
+            self.pickupCoordinates = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            
+            
+        }
+    }
 }
 
+enum TripState: Int {
+    case requested
+    case accepted
+    case inProgress
+    case completed
+}
