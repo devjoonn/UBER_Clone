09decbee9ec2339c503c82b0d88bd161f5a199b4
Hyeonjoonn <phj3046@gmail.com>
2023-06-26 11:27:08 +0900

    add: PickupViewController 추가

---
+++ UberClone/ViewController/PickupViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PickupViewController.swift
+//  UberClone
+//
+//  Created by 박현준 on 2023/06/26.
+//
+
+import UIKit
+
+class PickupViewController: UIViewController {
+
+//MARK: - UI Components
+    
+    
+//MARK: - Life cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
