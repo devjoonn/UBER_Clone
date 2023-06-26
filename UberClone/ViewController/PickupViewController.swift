18af3812270d01e39586e3696a791715f57837fa
Hyeonjoonn <phj3046@gmail.com>
2023-06-26 11:30:30 +0900

    feat: PickupViewController init - trip 생성

--- UberClone/ViewController/PickupViewController.swift
+++ UberClone/ViewController/PickupViewController.swift
@@ -6,13 +6,26 @@
 //
 
 import UIKit
+import MapKit
 
 class PickupViewController: UIViewController {
 
-//MARK: - UI Components
+//MARK: - Properties
+    private let mapView = MKMapView()
     
+    let trip: Trip
     
 //MARK: - Life cycles
+    
+    init(trip: Trip) {
+        self.trip = trip
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
