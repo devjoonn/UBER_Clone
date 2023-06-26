e719737eb04de4836ed934091453f97756fe272a
Hyeonjoonn <phj3046@gmail.com>
2023-06-27 02:12:22 +0900

    feat: PickupView base UI 구현

--- UberClone/ViewController/PickupViewController.swift
+++ UberClone/ViewController/PickupViewController.swift
@@ -12,12 +12,31 @@ import SnapKit
 class PickupViewController: UIViewController {
 
 //MARK: - Properties
-    private let mapView = MKMapView()
+    private let mapView: MKMapView = {
+        $0.layer.cornerRadius = 270 / 2
+        return $0
+    }(MKMapView())
     let trip: Trip
     
     private let cancelButton: UIButton = {
         $0.setImage(UIImage(named: "baseline_clear_white"), for: .normal)
         $0.addTarget(self, action: #selector(handleDismissal), for: .touchUpInside)
+        return $0
+    }(UIButton())
+    
+    private let pickupLabel: UILabel = {
+        $0.text = "Would you like to pickup this passenger?"
+        $0.font = UIFont.systemFont(ofSize: 16)
+        $0.textColor = .white
+        return $0
+    }(UILabel())
+    
+    private let acceptTripButton: UIButton = {
+        $0.backgroundColor = .white
+        $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        $0.setTitle("ACCEPT TRIP", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+        $0.addTarget(self, action: #selector(handleAcceptTrip), for: .touchUpInside)
         return $0
     }(UIButton())
     
@@ -43,10 +62,27 @@ class PickupViewController: UIViewController {
     func configureUI() {
         view.backgroundColor = .backgroundColor
         view.addSubview(cancelButton)
+        view.addSubview(mapView)
+        view.addSubview(pickupLabel)
+        view.addSubview(acceptTripButton)
         
         cancelButton.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide)
             make.leading.equalTo(16)
+        }
+        mapView.snp.makeConstraints { make in
+            make.width.height.equalTo(270)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(115)
+        }
+        pickupLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(mapView.snp.bottom).inset(-50)
+        }
+        acceptTripButton.snp.makeConstraints { make in
+            make.top.equalTo(pickupLabel.snp.bottom).inset(-16)
+            make.leading.trailing.equalToSuperview().inset(32)
+            make.height.equalTo(50)
         }
     }
     
@@ -55,4 +91,8 @@ class PickupViewController: UIViewController {
         dismiss(animated: true)
     }
 
+    @objc func handleAcceptTrip() {
+        print("DEBUG: accept Trip")
+    }
+    
 }
