af9ee252e3f3cab038effae708c713c6fea4ca6b
Hyeonjoonn <phj3046@gmail.com>
2023-07-29 11:29:15 +0900

    add: 원형 레이어 뷰 애니메이션 코드 추가

---
+++ UberClone/View/CircularProgressView.swift
@@ -0,0 +1,67 @@
+//
+//  CircularProgressView.swift
+//  UberClone
+//
+//  Created by 박현준 on 2023/07/29.
+//
+
+import UIKit
+
+class CircularProgressView: UIView {
+    
+//MARK: - Properties
+
+    var progressLayer: CAShapeLayer!
+    var trackLayer: CAShapeLayer!
+    var pulsatingLayer: CAShapeLayer!
+    
+//MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+//MARK: - Helper Functions
+    // 원형 애니메이션 코드
+    private func configureCircleLayers() {
+        pulsatingLayer = circleShapeLayer(strokeColor: .clear, fillColor: .blue)
+        layer.addSublayer(pulsatingLayer)
+        
+        trackLayer = circleShapeLayer(strokeColor: .clear, fillColor: .clear)
+        layer.addSublayer(trackLayer)
+        // end지점에서 멈춤
+        trackLayer.strokeEnd = 1
+        
+        progressLayer = circleShapeLayer(strokeColor: .systemPink, fillColor: .clear)
+        layer.addSublayer(progressLayer)
+        progressLayer.strokeEnd = 1
+    }
+    
+    // 원 레이어 잡기
+    private func circleShapeLayer(strokeColor: UIColor, fillColor: UIColor) -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        
+        let center = CGPoint(x: 0, y: 32)
+        // 12시부터 원형으로 돌기
+        let circularPath = UIBezierPath(arcCenter: center,
+                                        radius: self.frame.width / 2.5,
+                                        startAngle: -(.pi / 2),
+                                        endAngle: 1.5 * .pi,
+                                        clockwise: true)
+        
+        layer.path = circularPath.cgPath
+        layer.lineWidth = 12
+        layer.strokeColor = strokeColor.cgColor
+        layer.fillColor = fillColor.cgColor
+        layer.lineCap = .round
+        layer.position = self.center
+        
+        return layer
+    }
+    
+}
+
--- UberClone/ViewController/HomeViewController.swift
+++ UberClone/ViewController/HomeViewController.swift
@@ -538,10 +538,13 @@ extension HomeViewController: CLLocationManagerDelegate {
         locationManager?.delegate = self
          
         switch CLLocationManager.authorizationStatus() {
+        case .denied:
+            print("DEBUG: 거절 됨")
+            showAlertForLocationAuthorization()
         case .notDetermined:
             print("DEBUG: 위치 정보 없을 때 권한 정보 물음")
             locationManager?.requestWhenInUseAuthorization()
-        case .restricted, .denied:
+        case .restricted:
             break
         case .authorizedAlways:
             print("DEBUG: 항상 위치정보 권한 사용중")
@@ -553,6 +556,26 @@ extension HomeViewController: CLLocationManagerDelegate {
         @unknown default:
             break
         }   
+    }
+    
+    // 거부 시 재 실행하면 권한 요청을 위한 설정으로 이동
+    func showAlertForLocationAuthorization() {
+        let alertController = UIAlertController(
+            title: "위치 권한 설정",
+            message: "이 앱에서 위치 정보를 사용하려면 위치 권한이 필요합니다. 설정에서 위치 권한을 허용해주세요.",
+            preferredStyle: .alert
+        )
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        let settingsAction = UIAlertAction(title: "설정으로 이동", style: .default) { _ in
+            // 위치 권한 설정 화면으로 이동
+            UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+        }
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(settingsAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
 }
 
